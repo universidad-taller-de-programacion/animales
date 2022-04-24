4c8b581047ccb1e1e200f7c37dcfda492050bc7c
silibestref <federicosilibestre@gmail.com>
2022-04-24 20:48:09 -0300

    Avance

--- test/animals.js
+++ test/animals.js
@@ -1,29 +1,61 @@
-const { expect } = require("chai")
+const assert = require("assert");
+const chai = require("chai");
+const spies = require("chai-spies");
+const { expect } = chai;
+const lista = require("./lista");
 
-describe('Lista de animales', () => {
-  it('es un objeto', () => {
-    expect(lista).to.be.a('Object')
-  })
+chai.use(spies);
 
-  describe('#perros', () => {
-    it('devuelve los animales que son perros', () => {
-      expect(lista.perros()).to.equal(perros)
-    })
-  })
 
-  describe('#gatos', () => {
-    it('devuelve los animales que son gatos', () => {
-      expect(lista.gatos()).to.equal(gatos)
-    })
-  })
+const animalesZoo = [
+  {
+    tipo: "perro",
+    nombre: "Tito",
+  },
+  {
+    tipo: "gato",
+    nombre: "Michi",
+  },
+  {
+    tipo: "Sapo",
+    nombre: "Pepe",
+  },
+  {
+    tipo: "loro",
+    nombre: "Paco",
+  },
+];
 
-  describe('#otros', () => {
-    it('devuelve los animales que no son perros ni gatos', () => {
-      expect(lista.otros()).to.equal(otros)
-    })
+lista.animales = [...animalesZoo];
 
-    it('determina los resultados utilizando Array.filter', () => {
-      expect(animales.filter).to.have.been.called()
-    })
-  })
-})
+describe("Lista de animales", () => {
+  it("es un objeto", () => {
+    expect(lista).to.be.a("Object");
+  });
+
+  describe("#perros", () => {
+    it("devuelve los animales que son perros", () => {
+      const perros = animalesZoo.filter(animal => animal.tipo === "perro");
+      expect(lista.perros()).to.eql(perros);
+    });
+  });
+
+  describe("#gatos", () => {
+    it("devuelve los animales que son gatos", () => {
+      const gatos = animalesZoo.filter(animal => animal.tipo === "gato");
+      expect(lista.gatos()).to.eql(gatos);
+    });
+  });
+
+  describe("#otros", () => {
+    it("devuelve los animales que no son perros ni gatos", () => {
+      const otros = animalesZoo.filter(animal => animal.tipo !== "perro" && animal.tipo != "gato");
+      expect(lista.otros()).to.eql(otros);
+    });
+
+    it("determina los resultados utilizando Array.filter", () => {
+      expect(animales.filter).to.have.been.called();
+    });
+  });
+});
+
